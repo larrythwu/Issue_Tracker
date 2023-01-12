03af9b8c754a22833a2697251a53af6b42ece999
Larry Wu <larrywu@Larrys-MacBook-Pro.local>
2023-01-11 23:12:01 -0500

    Updates

---
+++ precache-manifest.9ba4cb4ce1fa696d6f9085c856730398.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1037fce81568cdc8698125aefdece935",
+    "url": "/Issue_Tracker/index.html"
+  },
+  {
+    "revision": "a9b90ef100b760f728ef",
+    "url": "/Issue_Tracker/static/css/2.80ecc7cd.chunk.css"
+  },
+  {
+    "revision": "f08f3e887c8aa14fdec0",
+    "url": "/Issue_Tracker/static/css/main.40c7bd83.chunk.css"
+  },
+  {
+    "revision": "a9b90ef100b760f728ef",
+    "url": "/Issue_Tracker/static/js/2.e7d8dbe1.chunk.js"
+  },
+  {
+    "revision": "8fc9e4c131f28ea99c1fba1405458c02",
+    "url": "/Issue_Tracker/static/js/2.e7d8dbe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08f3e887c8aa14fdec0",
+    "url": "/Issue_Tracker/static/js/main.df4097ef.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Issue_Tracker/static/js/main.df4097ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e80555d4bf9fbbb51ef",
+    "url": "/Issue_Tracker/static/js/runtime-main.77a8fa13.js"
+  },
+  {
+    "revision": "d5b40eaa648e3fa35169436c67d3175b",
+    "url": "/Issue_Tracker/static/media/js_logo.d5b40eaa.jpg"
+  },
+  {
+    "revision": "d3cc3e77ad5a26264f07e7f719a93002",
+    "url": "/Issue_Tracker/static/media/mongodb_logo.d3cc3e77.png"
+  },
+  {
+    "revision": "c4dbb03066ead9ec372f143481f04700",
+    "url": "/Issue_Tracker/static/media/nodejs_logo.c4dbb030.png"
+  },
+  {
+    "revision": "22808bae77c66bd1b33e298ea2f4320e",
+    "url": "/Issue_Tracker/static/media/react_logo.22808bae.png"
+  },
+  {
+    "revision": "c4f226e94861c395866a9b2c3bacb482",
+    "url": "/Issue_Tracker/static/media/ticket_icon.c4f226e9.png"
+  }
+]);
