308f45df8bc6f7864c6e52f530dbe9f48420b0fc
Larry Wu <larrywu@Larrys-MacBook-Pro.local>
2023-01-12 18:49:45 -0500

    debugging

--- server.js
+++ server.js
@@ -33,12 +33,13 @@ app.listen(PORT, () => {
 // app.use("/todo", require("./routes/todoRouter"));
 // app.use("/generaltext", require("./routes/textRouter"));
 // app.use(express.static("./client/build"));
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
- }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 //refer to index.js in routes folder
 const routes = require("./routes");
